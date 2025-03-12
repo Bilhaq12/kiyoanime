feffcf955828712505129ec4160489c5d9ec142b
bilhaq12 <iboysgntg@gmail.com>
2025-03-12 14:18:08 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  // Tambahkan ini untuk mengabaikan ESLint errors juga
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+    images: {
+        domains: ['cdn.myanimelist.net', 'img1.ak.crunchyroll.com', 'img.youtube.com', 'samehadaku.mba'],
+      },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
